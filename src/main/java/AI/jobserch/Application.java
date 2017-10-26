e735887ef9eb612cf4ffe3babb0dce796f312370
archana.achhra <archana.achhra@CPX-RUXC31DWRE7.dir.svc.accenture.com>
2017-10-26 18:59:20 +0100

    Change-Id: I0000000000000000000000000000000000000000

--- src/main/java/AI/jobserch/Application.java
+++ src/main/java/AI/jobserch/Application.java
@@ -2,49 +2,40 @@ package AI.jobserch;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.StringReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.stream.JsonReader;
 import com.ibm.watson.developer_cloud.personality_insights.v3.PersonalityInsights;
 import com.ibm.watson.developer_cloud.personality_insights.v3.model.Content;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.ContentItem;
 import com.ibm.watson.developer_cloud.personality_insights.v3.model.Profile;
 import com.ibm.watson.developer_cloud.personality_insights.v3.model.ProfileOptions;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.Trait;
 import com.ibm.watson.developer_cloud.util.GsonSingleton;
  
  
 @SpringBootApplication(scanBasePackages={"AI.jobserch"})
-public class Application implements CommandLineRunner {
+public class Application  {
  
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
     
  
-    public void run(String... strings) throws Exception {
-    	
-    	 PersonalityInsights service = new PersonalityInsights("2017-10-13","ce431ce2-c7b3-45a3-8481-3963a6556b47", "IWqKLZzIuKRM");
-		 Map<String, String> headers = new HashMap<String, String>();
-		 headers.put("X-Watson-Learning-Opt-Out", "true");
-		 service.setDefaultHeaders(headers);
-		 
-		 try {
-			  JsonReader jReader = new JsonReader(new FileReader("C:\\Users\\archana.achhra\\profile.json"));
-			  Content content =
-					  GsonSingleton.getGson().fromJson(jReader, Content.class);
-			  ProfileOptions options = new ProfileOptions.Builder()
-			    .contentItems(content.getContentItems())
-			    .consumptionPreferences(true)
-			    .rawScores(true)
-			    .build();
-			  Profile profile = service.getProfile(options).execute();
-			  System.out.println(profile);
-			} catch (FileNotFoundException e) {
-			  e.printStackTrace();
-			}
-    }
+    
+	
 }
--- src/main/java/AI/jobserch/RestApiController.java
+++ src/main/java/AI/jobserch/RestApiController.java
@@ -3,39 +3,35 @@
  */
 package AI.jobserch;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.net.URI;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
-/**
- * @author archana.achhra
- *
- */
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import com.google.gson.stream.JsonReader;
 import com.ibm.watson.developer_cloud.personality_insights.v3.PersonalityInsights;
 import com.ibm.watson.developer_cloud.personality_insights.v3.model.Content;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.ContentItem;
 import com.ibm.watson.developer_cloud.personality_insights.v3.model.Profile;
 import com.ibm.watson.developer_cloud.personality_insights.v3.model.ProfileOptions;
-import com.ibm.watson.developer_cloud.util.GsonSingleton;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.Trait;
  
 
  
@@ -44,21 +40,23 @@ import com.ibm.watson.developer_cloud.util.GsonSingleton;
 public class RestApiController {
 	 public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
 	 
-	// https://watson-api-explorer.mybluemix.net/apis/personality-insights-v3  
 	 @RequestMapping(value = "/jobSerach", method = RequestMethod.GET)
-	    public void getJobs(@RequestParam(value="name") String name ,@RequestParam(value="desc") String userDesc) {
+	    public ResponseEntity<List<LinkedHashMap<String, String>>>getJobs(@RequestParam(value="name") String name ,@RequestParam(value="desc") String userDesc) {
 		
 		 
-		 
+		 List<String> jobs = new ArrayList<String>();
 		 PersonalityInsights service = new PersonalityInsights("2017-10-13","ce431ce2-c7b3-45a3-8481-3963a6556b47", "IWqKLZzIuKRM");
 		 Map<String, String> headers = new HashMap<String, String>();
 		 headers.put("X-Watson-Learning-Opt-Out", "true");
 		 service.setDefaultHeaders(headers);
 		 
 		 try {
-			  JsonReader jReader = new JsonReader(new FileReader("C:\\Users\\archana.achhra\\profile.json"));
-			  Content content =
-					  GsonSingleton.getGson().fromJson(jReader, Content.class);
+			 Content content = new Content();
+			  ContentItem item=  new ContentItem();
+			  item.setContent(userDesc);
+			  item.setContentType("text/plain");
+			  item.setLanguage("en");
+			  content.addContentItem(item);	
 			  ProfileOptions options = new ProfileOptions.Builder()
 			    .contentItems(content.getContentItems())
 			    .consumptionPreferences(true)
@@ -66,10 +64,50 @@ public class RestApiController {
 			    .build();
 			  Profile profile = service.getProfile(options).execute();
 			  System.out.println(profile);
-			} catch (FileNotFoundException e) {
+			  //callJobAlogorithm(profile);
+			  
+			  List<LinkedHashMap<String, String>> jobmap = new ArrayList<LinkedHashMap<String, String>>();
+			  LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+			  map.put("Job Name", "Teacher");
+			  map.put("Job Description", "This is first job");
+			  map.put("Match percentage", "80%");
+			  jobmap.add(map);
+			  return new ResponseEntity<List<LinkedHashMap<String, String>>>(jobmap, HttpStatus.OK);
+			} catch (Exception e) {
 			  e.printStackTrace();
 			}
-		// return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+		return null;
+		
+	    }
+	 
+public void callJobAlogorithm(Profile profile){
+		 
+		 final String uri = new String("http://localhost:8080/api/jobs");
+		    RestTemplate restTemplate = new RestTemplate();
+		    // Add the Jackson message converter
+		    restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		    
+
+		    // set headers
+		    HttpHeaders headers = new HttpHeaders();
+		    headers.setContentType(MediaType.APPLICATION_JSON);
+		    headers.set("Authorization", "Basic " + "xxxxxxxxxxxx");
+		    HttpEntity<Profile> entity = new HttpEntity<Profile>(profile, headers);
+
+		    // send request and parse result
+		    ResponseEntity<String> response = restTemplate
+		            .exchange(uri, HttpMethod.POST, entity, String.class);
+
+		    
+		 
+		 
+	 }
+	 @RequestMapping(value = "/jobs", method = RequestMethod.POST)
+	    @ResponseBody
+	    public void updateCustomer(@RequestBody String s) {
+		 System.out.println("HI..." +s);
+	        
+	       
 	    }
 	
 }
