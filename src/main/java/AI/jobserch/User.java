f9b375bda9c83e3e182609e975b586fd2a7209da
archana.achhra <archana.achhra@CPX-RUXC31DWRE7.dir.svc.accenture.com>
2017-10-27 14:01:52 +0100

    Change-Id: I0000000000000000000000000000000000000000

---
+++ src/main/java/AI/jobserch/User.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package AI.jobserch;
+
+/**
+ * @author archana.achhra
+ *
+ */
+public class User {
+	
+	private String name;
+	private String desc;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
+
+}
